8611df93bca70ca07b22e1439f1f818ce8a31e05
sayyyeah <127043537+sayyyeah@users.noreply.github.com>
2024-02-14 22:07:40 +0300

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['158.160.7.78', '127.0.0.1', 'localhost', 'sayyyeahhostname.ddns.net']
+ALLOWED_HOSTS = ['158.160.7.78', '127.0.0.1', 'localhost', 'mytaskisprint.ddns.net']
 
 
 # Application definition
